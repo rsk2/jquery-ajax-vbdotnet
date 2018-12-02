e685eb1b189e73eaeb317c0d768e9edf82b9dd18
rohan <rsureshkumar@empyra.com>
2018-12-02 17:52:32 +0530

    Added ajax request javascript. Changes in web service methods

--- AjaxSampleWebApp/AjaxSampleWebApp/Base.aspx
+++ AjaxSampleWebApp/AjaxSampleWebApp/Base.aspx
@@ -5,14 +5,39 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
 </head>
 <body>
     <form id="form1" runat="server">
     <div id="Items">
-    
+        
+        <div class ="container" style="background-color:lightskyblue">
+            <div class ="row">
+                <div class = "col-sm-2">
+                    Hi
+                </div>
+            </div>
+        </div>
     </div>
     </form>
 </body>
-
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript">
+        $('#Items').mouseover(function () {
+            var url = "WebMethods/Items.asmx/GetNextFiveItems";
+            $.ajax({
+                url: url,
+                type: "GET",
+                contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    var jo = $.parseJSON(result.d);
+                    for (var key in jo) {
+                            var div = $("<div>"+jo[key]+"</div>")
+                            $('.container').append(div);
+                        }
+                    }
+            });
+        });
+</script>
 
 </html>
--- AjaxSampleWebApp/AjaxSampleWebApp/WebMethods/Items.asmx.vb
+++ AjaxSampleWebApp/AjaxSampleWebApp/WebMethods/Items.asmx.vb
@@ -4,89 +4,39 @@ Imports System.ComponentModel
 Imports System.Web.Script.Serialization
 
 
-' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
-' <System.Web.Script.Services.ScriptService()> _
+' To prohibit this Web Service to be called from script, using ASP.NET AJAX, comment the following line.
+<System.Web.Script.Services.ScriptService()> _
 <System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
 <System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
 <ToolboxItem(False)> _
 Public Class Items
     Inherits System.Web.Services.WebService
 
-    <WebMethod()> _
-    Public Function HelloWorld() As String
-       Return "Hello World"
-    End Function
+    Dim inventory As New List(Of String)(New String() {"Item Created", "Item Attended", "Item Consumed", "Item Released",
+                                         "Item Missed", "Item Registered", "Item Deleted", "Item Submitted",
+                                          "Item Purchased", "Item Received"})
 
-    <WebMethod(Description:="Get the next 10 timeline items", EnableSession:=True)> _
-    <Script.Services.ScriptMethod(ResponseFormat:=Script.Services.ResponseFormat.Json, XmlSerializeString:=False)>
-    Public Function GetNextTenTimelineItems(ByVal request As Dictionary(Of String, String) As String
-        Dim response As New Response
+    <WebMethod(Description:="Get the next 5 items", EnableSession:=True)> _
+    <Script.Services.ScriptMethod(ResponseFormat:=Script.Services.ResponseFormat.Json, XmlSerializeString:=False, UseHttpGet:=True)>
+    Public Function GetNextFiveItems() As String
         Dim jss As New JavaScriptSerializer()
-        'Dim tSearch As New TimelineItemFilter
-        'tSearch.MaxRows = 10
-        Dim maxIdString As String = "" ', maxItemDateTime As String = ""
-        Dim maxId As Integer
-        Dim entityTypeId As String = "", entityId As String = ""
-        If (request.TryGetValue("EntityId", entityId)) Then
-            tSearch.EntityId = Convert.ToInt32(entityId)
-        Else
-            response = New Response() With {.Status = False, .ErrorMsg = "Nonexistent key 'EntityId'"}
-            Return jss.Serialize(response)
-        End If
-        'If (request.TryGetValue("EntityTypeId", entityTypeId)) Then
-        '    tSearch.EntityTypeId = Convert.ToInt32(entityTypeId)
-        'Else
-        '    response = New Response() With {.Status = False, .ErrorMsg = "Nonexistent key 'EntityTypeId'"}
-        '    Return jss.Serialize(response)
-        'End If
-        'If (request.Data.TryGetValue("MaxItemDateTime", maxItemDateTime)) Then
-        '    tSearch.MaxItemDateTime = CDate(maxItemDateTime)
-        'Else
-        '    response = New Response() With {.Status = False, .ErrorMsg = "Nonexistent key 'MaxItemDateTime'"}
-        '    Return jss.Serialize(response)
-        'End If
-        If (request.TryGetValue("MaxItemId", maxIdString)) Then
-            maxId = Convert.ToInt32(maxIdString)
-        Else
-            response = New Response() With {.Status = False, .ErrorMsg = "Nonexistent key 'ItemId'"}
-            Return jss.Serialize(response)
-        End If
-        Dim tb As New TimelineBusiness(ServiceInterface.GetRequestContextFromWebApplication())
-        Dim tItemResults() As TimelineItem = tb.GetTimelineItem(tSearch)
-        Return jss.Serialize(tItemResults)
-        response = New Response With {.Status = False, .ErrorMsg = "Not logged in."}
-        Return jss.Serialize(response)
+        Dim response As Dictionary(Of String, String)
+        Dim itemResults = GetNextFiveFromInventory()
+        Return jss.Serialize(itemResults)
     End Function
 
-    Public Function GetNextFiveItems() As List(Of String)
-        Dim inventory As New List(Of String)(New String() {"Item Created", "Item Attended", "Item Consumed", "Item Released",
-                                              "Item Missed", "Item Registered", "Item Deleted", "Item Submitted",
-                                               "Item Purchased", "Item Sold"})
-        Dim list As New List(Of String())
-
+    Public Function GetNextFiveFromInventory() As List(Of String)
+        Dim copyOfInventory = inventory
+        Dim list As New List(Of String)
+        Dim randomIndex As Integer
+        Dim i = 1
+        While i <= 5
+            randomIndex = CInt(Math.Ceiling(Rnd() * (copyOfInventory.Count - 1)))
+            list.Add(copyOfInventory(randomIndex))
+            copyOfInventory.RemoveAt(randomIndex)
+            i += 1
+        End While
         Return list
     End Function
-
-End Class
-
-
-
-
-Public Class Request
-
-    Public Property Data As Dictionary(Of String, String)
-
-End Class
-
-Public Class Response
-
-    Public Property Status As Boolean
-    Public Property ErrorMsg As String
-    Public Property Result As Object
-
-    Public Sub New()
-        Status = True
-    End Sub
-
 
 End Class
